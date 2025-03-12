48f707ff437561641b4f9594262ecd8b826bd255
EmmanuelleCloutier <darkforest1045@gmail.com>
2025-03-12 11:30:50 -0400

    bug projet complet

--- Source/SnapshotSafari/Private/BaseProject/SnapshotSafari.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BaseProject/SnapshotSafari.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SnapshotSafari, "SnapshotSafari" );
- 
